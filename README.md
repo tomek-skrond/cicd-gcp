ff5377a283822243e1632717a80d0489980522da
Tomasz Skrond <tomek2.skrond@gmail.com>
2023-09-24 01:36:43 +0200

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+### cicd-gcp
+Project I used to get started understanding basic Cloud Build concepts.
+Code is forked from the `https://github.com/guillaumeVilar/ci-cd-gcp-example/tree/main` repo.
--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,32 @@
-
 provider "google" {
   credentials = file(var.credential_file)
   project     = var.project
   region      = var.region
   zone        = var.zone
+}
+
+
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = "my-repository"
+  description   = "example docker repository"
+  format        = "DOCKER"
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
 }
